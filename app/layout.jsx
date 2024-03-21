e13f14a47ad51e150d2b06ab3c40d196b511d911
Daniel <121930995+danielcodes-xyz@users.noreply.github.com>
2024-03-21 06:44:15 -0700

    Added some components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,23 @@
+import Navbar from '@/components/Navbar';
 import '@/assets/styles/global.css';
+import Footer from '@/components/Footer';
 
 export const metadata = {
-    title: 'Property Pulse | Find The Perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties',
-}
+  title: 'Property Pulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-        <body>
-            <div>{children}</div>
-        </body>
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 };
 
-export default MainLayout
+export default MainLayout;
