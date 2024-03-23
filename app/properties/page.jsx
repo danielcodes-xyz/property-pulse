cb022effa4b2560ea2453086705542b5496e8fb4
Daniel <121930995+danielcodes-xyz@users.noreply.github.com>
2024-03-23 09:44:42 -0700

    Trouble fetching data with api

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,9 @@
-import properties from '@/properties.json';
+import PropertyCard from '@/components/PropertyCard';
+import { fetchProperties } from '@/utils/requests';
 
-const PropertiesPage = () => {
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
@@ -8,9 +11,9 @@ const PropertiesPage = () => {
           <p>No Properties found</p>
         ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {properties.map((property) => {
-              <div>{property.name}</div>;
-            })}
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
           </div>
         )}
       </div>
